5ebc7dd19354c65de50d894a08b248b90106bbf7
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-05-26 17:29:01 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,36 +1,39 @@
 // Copyright (c) 2025 Ain Jeong All rights reserved
 //
 // Created by: Ain Jeong
-// Created on: May 2025
+// Created on: June 2025
 // This file contains the JS functions for index.html
 
 'use strict'
 /**
- * This function gets the weather today
+ * This function checks the random facts of extinct animals
  * The 'async' is there because it will take time for the internet to return the value
  */
-async function weatherToday () {
+async function checkRandomFacts () {
   // the 'try' is here because the internet may not be working
   // it is like an 'if ... else' statement'
   try {
     const resultJSON = await fetch(
-      'https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5'
+      'https://extinct-api.herokuapp.com/api/v1/animal/'
     )
     const jsonData = await resultJSON.json()
     console.log(jsonData)
 
     // bring the information from API
-    const iconCode = jsonData.weather[0].icon
-    const weatherMain = jsonData.weather[0].main
-    const weatherDescription = jsonData.weather[0].description
-    const tempKelvin = jsonData.main.temp
-    const tempCelsius = tempKelvin - 273.15
+    const imageSrc = jsonData.data[0].imageSrc
+    const commonName = jsonData.data[0].commonName
+    const binomialName = jsonData.data[0].binomialName
+    const location = jsonData.data[0].location
+    const lastRecord = jsonData.data[0].lastRecord
+    const shortDescription = jsonData.data[0].shortDesc
 
     // output
-    document.getElementById('weather-icon').src = 'https://openweathermap.org/img/wn/' + iconCode + '@2x.png'
-    document.getElementById('weather-today').innerHTML =
-      'Weather: ' + weatherMain + '; ' + weatherDescription + '<br />' +
-      'Temperature: ' + tempCelsius.toFixed(1) + ' °C'
+    document.getElementById('random-image').src = imageSrc
+    document.getElementById('extinct-animal-facts').innerHTML =
+      'Name: ' + commonName + '(' + binomialName + ')<br />' +
+      'Location: ' + location + '<br />' +
+      'Last Record: ' + lastRecord + '<br />' +
+      'Short Description: ' + shortDescription + '<br />'
   } catch (error) {
     console.error(error)
   }
